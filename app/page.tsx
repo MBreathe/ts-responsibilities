0683dc8804f61e7422ee27520aecd6ce9ada4811
Ilia Bubnov <makebreathe1224@gmail.com>
2026-09-14 17:21:14 +0200

    bare base

--- app/page.tsx
+++ app/page.tsx
@@ -1,70 +1,125 @@
-import Image from 'next/image';
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+    Item,
+    ItemActions,
+    ItemContent,
+    ItemDescription,
+    ItemGroup,
+    ItemTitle,
+} from '@/components/ui/item';
+import { PencilIcon, TrashIcon } from '@phosphor-icons/react';
 
 export default function Home() {
+    const unassignedTasks = [
+        { name: 'Task1', description: 'Task1 description' },
+        { name: 'Task2', description: 'Task2 description' },
+        { name: 'Task3', description: 'Task3 description' },
+    ];
+    const users = [{ name: 'Ilya' }, { name: 'Ilyas' }];
+    const tasksIlya = [
+        { name: 'Task4', description: 'Task4 description' },
+        { name: 'Task5', description: 'Task5 description' },
+        { name: 'Task6', description: 'Task6 description' },
+    ];
+    const tasksIlyas = [
+        { name: 'Task7', description: 'Task7 description' },
+        { name: 'Task8', description: 'Task8 description' },
+        { name: 'Task9', description: 'Task9 description' },
+    ];
+
     return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <Image
-                    className="dark:invert h-5 w-[100px]"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={100}
-                    height={20}
-                    priority
-                />
-                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                        To get started, edit the{' '}
-                        <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-                            page.tsx
-                        </code>{' '}
-                        file.
-                    </h1>
-                    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                        Looking for a starting point or more instructions? Head
-                        over to{' '}
-                        <a
-                            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
-                        >
-                            Templates
-                        </a>{' '}
-                        or the{' '}
-                        <a
-                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
-                        >
-                            Learning
-                        </a>{' '}
-                        center.
-                    </p>
-                </div>
-                <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-                    <a
-                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert h-[14px] w-4"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={16}
-                            height={14}
-                        />
-                        Deploy Now
-                    </a>
-                    <a
-                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Documentation
-                    </a>
-                </div>
-            </main>
+        <div className="p-5">
+            <h1 className="p-2">Unassigned:</h1>
+            <ItemGroup>
+                {unassignedTasks.map((item, index) => {
+                    return (
+                        <Item variant="outline" key={index}>
+                            <ItemContent>
+                                <ItemTitle>{item.name}</ItemTitle>
+                                <ItemDescription>
+                                    {item.description}
+                                </ItemDescription>
+                            </ItemContent>
+                            <ItemActions>
+                                <Button variant="default" size="sm">
+                                    <PencilIcon />
+                                </Button>
+                                <Button variant="destructive" size="sm">
+                                    <TrashIcon />
+                                </Button>
+                            </ItemActions>
+                        </Item>
+                    );
+                })}
+            </ItemGroup>
+
+            {users.map((user, index) => {
+                return (
+                    <div key={index}>
+                        <h1 className="p-2">{user.name}:</h1>
+                        <ItemGroup>
+                            {user.name === 'Ilya'
+                                ? tasksIlya.map((item, index) => {
+                                      return (
+                                          <Item variant="outline" key={index}>
+                                              <ItemContent>
+                                                  <ItemTitle>
+                                                      {item.name}
+                                                  </ItemTitle>
+                                                  <ItemDescription>
+                                                      {item.description}
+                                                  </ItemDescription>
+                                              </ItemContent>
+                                              <ItemActions>
+                                                  <Button
+                                                      variant="default"
+                                                      size="sm"
+                                                  >
+                                                      <PencilIcon />
+                                                  </Button>
+                                                  <Button
+                                                      variant="destructive"
+                                                      size="sm"
+                                                  >
+                                                      <TrashIcon />
+                                                  </Button>
+                                              </ItemActions>
+                                          </Item>
+                                      );
+                                  })
+                                : tasksIlyas.map((item, index) => {
+                                      return (
+                                          <Item variant="outline" key={index}>
+                                              <ItemContent>
+                                                  <ItemTitle>
+                                                      {item.name}
+                                                  </ItemTitle>
+                                                  <ItemDescription>
+                                                      {item.description}
+                                                  </ItemDescription>
+                                              </ItemContent>
+                                              <ItemActions>
+                                                  <Button
+                                                      variant="default"
+                                                      size="sm"
+                                                  >
+                                                      <PencilIcon />
+                                                  </Button>
+                                                  <Button
+                                                      variant="destructive"
+                                                      size="sm"
+                                                  >
+                                                      <TrashIcon />
+                                                  </Button>
+                                              </ItemActions>
+                                          </Item>
+                                      );
+                                  })}
+                        </ItemGroup>
+                    </div>
+                );
+            })}
         </div>
     );
 }
