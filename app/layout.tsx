07cffd06e2cc6c6d7df4ebcbd30aecd620cb1696
Ilia Bubnov <makebreathe1224@gmail.com>
2026-09-21 13:25:22 +0200

    metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type Metadata from 'next';
+import { Metadata } from 'next';
 import { Geist, Geist_Mono, JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
