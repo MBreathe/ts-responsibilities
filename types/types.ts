236817d6934095582c42f8285d4ce2db430fb195
Ilia Bubnov <makebreathe1224@gmail.com>
2026-09-23 16:32:53 +0200

    upd deps + types

--- types/types.ts
+++ types/types.ts
@@ -1,1 +1,26 @@
 export type Params = { params: Promise<{ id: string }> };
+
+export type User = {
+    _id: string;
+    name: string;
+    assignedTasks: string[];
+};
+
+type Weight = 0 | 1 | 2;
+
+export type Task = {
+    _id: string;
+    title: string;
+    createdBy: string;
+    assignedTo: string[];
+    room: string[];
+    weight: Weight;
+    description: string;
+    createdOn: string;
+};
+
+export type Room = {
+    _id: string;
+    name: string;
+    assignedTasks: string[];
+};
